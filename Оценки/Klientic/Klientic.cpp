831f0dab7aed0a3cfd16bf616cbe9a3311123f6d
Kurkumich <113376198+Kurkumich@users.noreply.github.com>
2022-11-26 22:11:42 +0300

    Add files via upload

---
+++ Оценки/Klientic/Klientic.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+struct Person {
+    char name[25];
+    int web;
+    int math;
+    int diff;
+    int disk;
+} persona;
+
+void main() {
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\kurku\\OneDrive\\Рабочий стол\\Оценки\\Файлы\\second.bin";
+    const char* nameA = "C:\\Users\\kurku\\OneDrive\\Рабочий стол\\Оценки\\Файлы\\twice.bin";
+    ofstream fR;
+    ifstream fA;
+    long pred_size;
+    int answer = 0;
+    while (true)
+    {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        cout << "Введите Ваше имя и оценки по 4 экзаменам: Web, Математический анализ, Дифференциальные уравнения и Дискретная математика:" << endl;
+        cin >> persona.name >> persona.web >> persona.math >> persona.diff >> persona.disk;
+        cout << "Ответ для пользователя: " << persona.name << endl;
+        fR.open(nameR, ios::app | ios::binary);
+        fR.write((char*)&persona, sizeof(persona));
+        fR.close();
+
+        fA.open(nameA, ios::binary);
+        if (!fA.is_open())
+            cout << "ERROR" << endl;
+        else
+        {
+            fA.seekg(0, ios::end);
+            pred_size = fA.tellg();
+            while (pred_size >= fA.tellg())
+            {
+                Sleep(100);
+                fA.seekg(0, ios::end);
+            }
+            fA.seekg(pred_size, ios::beg);
+            fA.read((char*)&answer, sizeof(answer));
+            fA.close();
+        }
+        switch (answer) {
+        case 0: {cout << "Вы сдали сессию, поздравляем! " << endl << endl; break; }
+        case 1: {cout << "К сожалению, Вы не сдали сессию( Кол-во долгов: 1! " << endl << endl; break; }
+        case 2: {cout << "К сожалению, Вы не сдали сессию( Кол-во долгов: 2! " << endl << endl; break; }
+        case 3: {cout << "К сожалению, Вы не сдали сессию( Кол-во долгов: 3! " << endl << endl; break; }
+        case 4: {cout << "К сожалению, Вы не сдали сессию( Кол-во долгов: 4! " << endl << endl; break; }
+        }
+    }
+}
---
+++ Оценки/Server/Server.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+struct Person {
+    char name[25];
+    int web;
+    int math;
+    int diff;
+    int disk;
+} persona2;
+
+int answer=0;
+long size_pred;
+const int n = 4;
+int mark[n];
+int dolg = 0;
+
+int main() {
+    ifstream fR;
+    ofstream fA;
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\kurku\\OneDrive\\Рабочий стол\\Оценки\\Файлы\\second.bin";
+    const char* nameA = "C:\\Users\\kurku\\OneDrive\\Рабочий стол\\Оценки\\Файлы\\twice.bin";
+
+    cout << "server is working" << endl;
+    fR.open(nameR, ios::binary);
+    fR.seekg(0, ios::end);
+    size_pred = fR.tellg();
+    fR.close();
+    while (true)
+    {
+        fR.open(nameR, ios::binary);
+        fR.seekg(0, ios::end);
+        while (size_pred >= fR.tellg())
+        {
+            Sleep(100); fR.seekg(0, ios::end);
+        }
+        fR.seekg(size_pred, ios::beg);
+        fR.read((char*)&persona2, sizeof(persona2));
+        cout << persona2.name << ", подождите Ваш запрос обрабатывается." << endl;
+        size_pred = fR.tellg();
+        fR.close();
+        mark[0] = persona2.web;
+        mark[1] = persona2.math;
+        mark[2] = persona2.diff;
+        mark[3] = persona2.disk;
+        for (int i = 0; i < n; i++)
+        {
+            if (mark[i] < 3)
+                dolg++;
+
+        }
+        if (dolg == 0) {
+            answer = 0;
+        }
+        if (dolg == 1) {
+            answer = 1;
+        }
+        if (dolg == 2) {
+            answer = 2;
+        }
+        if (dolg == 3) {
+            answer = 3;
+        }
+        if (dolg == 4) {
+            answer = 4;
+        }
+        fA.open(nameA, ios::binary | ios::app);
+        fA.write((char*)&answer, sizeof(answer));
+        fA.close();
+        for (int i = 0; i < n; i++)
+        {
+            mark[i] = 0;
+        }
+        dolg = 0;
+    }
+}
